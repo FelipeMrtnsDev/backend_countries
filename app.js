653bccd607aec428a29027cb4608d55a85a19059
felipe martins <juninpernadecano@gmail.com>
2024-09-11 09:54:10 -0400

    v2 commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const countries = require('./routes/countries'); // Corrigir o caminho para o arquivo de rotas
+const countries = require('./routes/countries');
 
 const app = express();
 const port = 8082;
@@ -8,7 +8,6 @@ app.get("/", (req, res) => {
     res.send({ message: 'Hello World' });
 });
 
-// Usar o prefixo de versão v1 para as rotas
 app.use("/v1", countries);
 
 app.listen(port, () => {
--- routes/countries.js
+++ routes/countries.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-// Endpoint para obter países disponíveis
 router.get('/api/countries', async (req, res) => {
     try {
         const response = await axios.get('https://date.nager.at/api/v3/AvailableCountries');
@@ -13,40 +12,32 @@ router.get('/api/countries', async (req, res) => {
     }
 });
 
-// Endpoint para obter informações detalhadas sobre um país
 router.get('/api/country/:countryCode', async (req, res) => {
     const countryCode = req.params.countryCode;
 
     try {
         console.log(`Fetching data for country code: ${countryCode}`);
         
-        // Obter informações sobre países vizinhos
         const borderCountriesResponse = await axios.get(`https://date.nager.at/api/v3/CountryInfo/${countryCode}`);
         console.log('Border Countries Response:', borderCountriesResponse.data);
 
-        // Obter dados populacionais
         const populationResponse = await axios.get('https://countriesnow.space/api/v0.1/countries/population');
         console.log('Population Response:', populationResponse.data);
 
-        // Obter URL da bandeira
         const flagResponse = await axios.get('https://countriesnow.space/api/v0.1/countries/flag/images');
         console.log('Flag Response:', flagResponse.data);
 
-        // Processar dados das respostas
         const borderCountries = borderCountriesResponse.data && borderCountriesResponse.data.borders
             ? borderCountriesResponse.data.borders
             : [];
         
-        // Encontrar dados populacionais para o país especificado (por nome)
         const countryName = borderCountriesResponse.data.commonName || "";
         const populationData = populationResponse.data && populationResponse.data.data
             ? populationResponse.data.data.find(c => c.country === countryName)
             : {};
         
-        // Formatando dados populacionais
         const formattedPopulationData = populationData ? populationData.populationCounts : [];
         
-        // Encontrar URL da bandeira para o país especificado
         const flagUrl = flagResponse.data && flagResponse.data.data
             ? flagResponse.data.data.find(c => c.name === countryName)?.flag
             : '';
